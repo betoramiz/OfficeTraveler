f4b15a1728dff3e3eb31d8ab1d41baecd399f428
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2022-10-04 23:08:45 -0500

    Added login controls

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { SinginComponent } from './singin/singin.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { SinginComponent } from './singin/singin.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
