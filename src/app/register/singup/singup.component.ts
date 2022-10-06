68c07fc57c29d7e6b621da112ce1524dffa57ca9
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2022-10-06 12:13:49 -0500

    changed to scss

--- src/app/register/singup/singup.component.ts
+++ src/app/register/singup/singup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
-  styleUrls: ['./singup.component.css']
+  styleUrls: ['./singup.component.scss']
 })
 export class SingupComponent implements OnInit {
 
