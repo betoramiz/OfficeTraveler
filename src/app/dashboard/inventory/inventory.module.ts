81655e70c8eba926c89510781ebcf4a51ec6ff19
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2022-10-13 10:12:44 -0500

    added office types layout

--- src/app/dashboard/inventory/inventory.module.ts
+++ src/app/dashboard/inventory/inventory.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { OfficesComponent } from './offices/offices.component';
 import { OfficeDetailComponent } from './office-detail/office-detail.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +17,11 @@ import { OfficeDetailComponent } from './office-detail/office-detail.component';
   ],
   imports: [
     CommonModule,
-    InventoryRoutingModule
+    InventoryRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule
   ]
 })
 export class InventoryModule { }
--- src/app/dashboard/inventory/offices/offices.component.ts
+++ src/app/dashboard/inventory/offices/offices.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface OfficeType {
+  name: string;
+  inventory: number;
+}
+
 @Component({
   selector: 'app-offices',
   templateUrl: './offices.component.html',
   styleUrls: ['./offices.component.css']
 })
 export class OfficesComponent implements OnInit {
+
+  dataSource: OfficeType[] = [
+    { name: 'Silla individual', inventory: 10 },
+    { name: 'Oficina Privada', inventory: 5 },
+    { name: 'Sala de Juntas', inventory: 2 }
+  ];
+  displayedColumns = ['name', 'inventory'];
 
   constructor() { }
 
